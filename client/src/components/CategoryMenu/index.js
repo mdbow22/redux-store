e0df02ad88ac0dd7ca287634aa35f918d1132c6c
mdbow22 <mdbow22@gmail.com>
2022-01-10 14:50:18 -0500

    commented out all old useContext code

--- client/src/components/CategoryMenu/index.js
+++ client/src/components/CategoryMenu/index.js
@@ -1,11 +1,11 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useQuery } from '@apollo/client';
-import { useStoreContext } from '../../utils/GlobalState';
-import {
+//import { useStoreContext } from '../../utils/GlobalState';
+/* import {
   UPDATE_CATEGORIES,
   UPDATE_CURRENT_CATEGORY,
-} from '../../utils/actions';
+} from '../../utils/actions'; */
 import { QUERY_CATEGORIES } from '../../utils/queries';
 import { idbPromise } from '../../utils/helpers';
 import { updateCategories, updateCurrentCategory } from '../../utils/redux/cartSlice';
--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -3,13 +3,13 @@ import { Link, useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 import { useDispatch, useSelector } from 'react-redux';
 import Cart from '../components/Cart';
-import { useStoreContext } from '../utils/GlobalState';
+/* import { useStoreContext } from '../utils/GlobalState';
 import {
   REMOVE_FROM_CART,
   UPDATE_CART_QUANTITY,
   ADD_TO_CART,
   UPDATE_PRODUCTS,
-} from '../utils/actions';
+} from '../utils/actions'; */
 import { QUERY_PRODUCTS } from '../utils/queries';
 import { idbPromise } from '../utils/helpers';
 import spinner from '../assets/spinner.gif';
@@ -20,7 +20,7 @@ import {
   updateCartQuantity } from '../utils/redux/cartSlice';
 
 function Detail() {
-  const [state, dispatch] = useStoreContext();
+  //const [state, dispatch] = useStoreContext();
   const { id } = useParams();
 
   //redux stuff
@@ -31,7 +31,7 @@ function Detail() {
 
   const { loading, data } = useQuery(QUERY_PRODUCTS);
 
-  const { products, cart } = state;
+  //const { products, cart } = state;
 
   useEffect(() => {
     // already in global store
