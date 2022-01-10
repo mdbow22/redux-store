1e07bd8744bee70770ff82ff96217abb1bfd5dd0
mdbow22 <mdbow22@gmail.com>
2022-01-09 21:26:29 -0500

    created redux store and slice for reducer

---
+++ client/src/utils/redux/cartSlice.js
@@ -0,0 +1,67 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const cartSlice = createSlice({
+    name: 'cart',
+    initialState: {
+        products: [],
+        cart: [],
+        cartOpen: false,
+        categories: [],
+        currentCategory: '',
+    },
+    reducers: {
+        updateProducts: (state, action) => {
+            state.products = [...state.products, action.products];
+        },
+        addToCart: (state, action) => { 
+            state.cartOpen = true; 
+            state.cart = [...state.cart, action.product];
+        },
+        addMultipleToCart: (state, action) => {
+            state.cart = [...state.card, ...action.products];
+        },
+        updateCartQuantity: (state, action) => {
+            state.cartOpen = true;
+            state.cart = state.cart.map((product) => {
+                if(action._id === product._id) {
+                    product.purchaseQuantity = action.purchaseQuantity;
+                }
+                return product;
+            })
+        },
+        removeFromCart: (state, action) => {
+            state.cart = state.cart.filter((product) => {
+                return product._id !== action._id;
+            });
+
+            state.cartOpen = state.cart.length > 0;
+        },
+        clearCart: state => {
+            state.cartOpen = false;
+            state.cart.splice(0, state.cart.length);
+        },
+        toggleCart: state => {
+            state.cartOpen = !state.cartOpen;
+        },
+        updateCategories: (state, action) => {
+            state.categories = [...action.categories];
+        },
+        updateCurrentCategory: (state, action) => {
+            state.currentCategory = action.currentCategory;
+        }
+    }
+});
+
+export const {
+    updateProducts,
+    addToCart,
+    addMultipleToCart,
+    updateCartQuantity,
+    removeFromCart,
+    clearCart,
+    toggleCart,
+    updateCategories,
+    updateCurrentCategory
+} = cartSlice.actions;
+
+export default cartSlice.reducer;
